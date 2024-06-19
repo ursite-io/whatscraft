3b57df266ccf852b1bbd34e676acb0dc299172c8
JEANCAAX <ivanjeancax@hotmail.com>
2024-06-18 23:47:24 -0600

    call to api, installing dependency in pom

--- src/main/java/com/ursite/App.java
+++ src/main/java/com/ursite/App.java
@@ -1,10 +1,38 @@
 package com.ursite;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+// import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 public class App extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Hello, SpigotMC!");
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost("http://localhost:3000/dev/whatscraft/sendMessage");
+            request.addHeader("Accept", "application/json");
+
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                HttpEntity entity = response.getEntity();
+
+                if (entity != null) {
+                    String result = EntityUtils.toString(entity);
+                    getLogger().info("MSG de API: " + result);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
     @Override
     public void onDisable() {
         getLogger().info("See you again, SpigotMC!");
